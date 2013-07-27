8bf24cec948dde38b4c008648951db6d87959491
Christophe Van Huele <christophe.vanhuele@gmail.com>
2013-07-27 15:09:28 +0200

    added data, solution csv file and modified read script

--- shiny/read.R
+++ shiny/read.R
@@ -1,5 +1,6 @@
 library("xlsx")
-
+setwd("~/src/accent/shiny/")
 patients <- read.xlsx2("data.xls", sheetName="patients", header=TRUE,as.data.frame=TRUE)
 therapists <- read.xlsx2("data.xls", sheetName="therapists", header=TRUE,as.data.frame=TRUE)
 links <- read.xlsx2("data.xls", sheetName="links", header=TRUE,as.data.frame=TRUE)
+head(patients)
