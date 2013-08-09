2598e74cf3b14c80998e525a097f470bd733891b
Kenny Helsens <kennyhelsens@gmail.com>
2013-08-09 18:33:09 +0200

    refactored AccentModelInput with patientskills

--- pkg/R/input.R
+++ pkg/R/input.R
@@ -5,24 +5,38 @@
 #' @param xlsFile
 #' 
 #' \itemize{
-#'  \item{sheet patients:}{Sepal width}
-#'  \item{sheet.therapist:}{Sepal Length}
+#'  \item{sheet patients:}{Sheet with patient information}
+#'  \item{sheet.therapist:}{Sheet with therapist information}
 #'  \item{sheet.links:}{Petal Width}
 #' }
 #' @export 
-#' @return  AccentModelInput instance. This instance contains 3 data.frames extracted from the given XLS file.
+#' @return  AccentModelInput instance. This instance contains 2 data.table instances extracted from the given XLS file.
+#' @example
+#' test.xls <- system.file(package="thinkdata.accent", "examples", "data.xls")
+#' input <- readXLSModelInput(xlsFile=test.xls) 
+#' isTRUE("AccentModelInput" %in% class(input))
+#' str(input)
 readXLSModelInput <- function(xlsFile){
 	require("xlsx")
 	
 	Log$info(sprintf("reading accent model input from %s ", xlsFile))
 
 	this <- list()
-	this$patients <- read.xlsx2(xlsFile, sheetName="patients")
-	this$therapists <- read.xlsx2(xlsFile, sheetName="therapists")
 	
+	this$patients <- data.table(read.xlsx2(xlsFile, sheetName="patients"))
+	setnames(this$patients, c("patient"))
+	
+	this$patientskills <- data.table(read.xlsx2(xlsFile, sheetName="patientskills"))
+	setnames(this$patientskills, c("patient", "skill"))
+	
+	this$therapists <- data.table(read.xlsx2(xlsFile, sheetName="therapists"))
+	setnames(this$therapists, c("therapist", "skill"))
+  
 	# TODO: this is not used? 
 	# this$links <- read.xlsx2(xlsFile, sheetName="links")
 
 	class(this) <- c("AccentModelInput")
 	this
 }
+
+
