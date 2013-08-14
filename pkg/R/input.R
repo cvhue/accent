51392742d0ba3f436afa3a2af7c2b19335bfd895
Kenny Helsens <kennyhelsens@gmail.com>
2013-08-14 08:36:44 +0200

    added random generator for AccentModelInput instance

--- pkg/R/input.R
+++ pkg/R/input.R
@@ -11,11 +11,13 @@
 #' }
 #' @export 
 #' @return  AccentModelInput instance. This instance contains 2 data.table instances extracted from the given XLS file.
-#' @example
-#' test.xls <- system.file(package="thinkdata.accent", "examples", "data.xls")
-#' input <- readXLSModelInput(xlsFile=test.xls) 
-#' isTRUE("AccentModelInput" %in% class(input))
-#' str(input)
+#' @example 
+#' \dontrun{
+#'   test.xls <- system.file(package="thinkdata.accent", "examples", "data.xls")
+#'   input <- readXLSModelInput(xlsFile=test.xls) 
+#'   isTRUE("AccentModelInput" %in% class(input))
+#'   str(input)
+#' }
 readXLSModelInput <- function(xlsFile){
 	require("xlsx")
 	
@@ -40,3 +42,46 @@ readXLSModelInput <- function(xlsFile){
 }
 
 
+#' @title generate a random accent model input
+#' @return  AccentModelInput instance. This instance contains 2 data.table instances extracted from the given XLS file.
+#' @export 
+#' @examples
+#' input <- randomAccentModelInput() 
+#' isTRUE("AccentModelInput" %in% class(input))
+#' str(input)
+randomAccentModelInput <- function(){
+  Log$info("creating random AccentModelInput instance")
+  patients <- data.table(patient=paste0("P", 1:as.integer(runif(n=1,min=5,max=10))))
+  patientskills <- data.table(
+    patient=paste0("P", as.integer(runif(n=nrow(patients),min=1,max=10))),
+    skill=as.integer(runif(n=nrow(patients)*2, min=1, max=4))
+    )
+  patientskills <- unique(patientskills)
+
+# FIXME: therapist can now only have one skill, this randomizer has multiple skills per therapist.
+#   therapists <- data.table(
+#     therapist=paste0("Therapist-", rep(LETTERS[1:3],3)),
+#     skill=as.integer(runif(9, 1,100)%%3)+1
+#   )  
+#   therapists <- unique(therapists)
+#   
+  therapists <- data.table(
+        therapist=LETTERS[1:3],
+        skill=1:3
+ )  
+    
+  
+  parameters <- data.table(
+    parameter=c("minhours", "preference"),
+    value=c(1, 0.5)
+  ) 
+
+  this <- list()
+  
+  this$patients <- patients
+  this$patientskills <- patientskills
+  this$therapists <- therapists
+  this$parameters <- parameters
+  class(this) <- c("AccentModelInput")
+  this
+}
