e30fcbe40097828b4b01f90a423c9f16a069a017
Kenny Helsens <kennyhelsens@gmail.com>
2013-09-03 17:36:58 +0200

    added example splan-json file that can be read as an accent input model

--- pkg/inst/shiny/accent/global.R
+++ pkg/inst/shiny/accent/global.R
@@ -2,13 +2,22 @@ library(thinkdata.accent)
 
 setLogging("info", logger="dateLogger")
 
+### RANDOM DATA
 # Read model and optimize solution
 # input <- randomAccentModelInput()
 
-test.json <- system.file(package="thinkdata.accent", "examples", "data.json")
-input <- readSimpleJSONModelInput(jsonFile=test.json) 
-str(input)
-cat(toJSON(input))
+### SPLAN JSON
+test.json <- system.file(package="thinkdata.accent", "examples", "splan_data.json")
+input <- readSplanJSONInput(jsonFile=test.json)
+
+### SIMPLE JSON
+# test.json <- system.file(package="thinkdata.accent", "examples", "data.json")
+# input <- readSimpleJSONModelInput(jsonFile=test.json) 
+
+# str(input)
+# str(splaninput)
+# cat(toJSON(input))
+
 solution <- optimizeAccentModel(input)
 
 therapistList = c("All", unique(input$therapists$therapist))
