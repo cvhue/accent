872226c1b3e933a492955e054cc51261909e3e1c
Christophe Van Huele <christophe.vanhuele@gmail.com>
2013-09-06 20:31:43 +0200

    updated test function  

--- pkg/tests/test_all.R
+++ pkg/tests/test_all.R
@@ -2,5 +2,9 @@ library(thinkdata.accent)
 
 if (require(testthat)){
    print("testing...")
-   test_package("thinkdata.accent")
+   #test_package("thinkdata.accent")
+   test.file <- system.file(file.path("examples","data.xls"),package="thinkdata.accent")
+   model <- thinkdata.accent::readXLSModelInput(test.file)
+   solution <- thinkdata.accent::optimize(model)
+   thinkdata.accent::schedule(read.csv(solution$file))
 }
