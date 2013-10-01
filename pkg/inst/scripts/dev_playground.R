6a1d8b8492a0bb9736fe6d310ebe77ada7947136
Kenny Helsens <kennyhelsens@gmail.com>
2013-10-01 16:41:48 +0200

    updated playground dev code

--- pkg/inst/scripts/dev_playground.R
+++ pkg/inst/scripts/dev_playground.R
@@ -1,3 +1,13 @@
 require(thinkdata.accent)
 runShinyAccent()
 
+
+test.json <- system.file(package="thinkdata.accent", "examples", "splan_data.json")
+input <- readSplanJSONInput(jsonFile=test.json)
+solution <- optimizeAccentModel(input)
+
+drawSchedule(solution, type="therapist")
+
+
+startService()
+req <- getLastRequest()
